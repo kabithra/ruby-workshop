c7c222f4331264faa70c64c7097b9d54c84f77b6
root <root@201011JUNC0398.(none)>
2015-03-05 15:09:52 +0530

    #Please enter the commit message for your changes. Lines starting

--- add.rb
+++ add.rb
@@ -1,6 +1,5 @@
 print 8+2
 print "\n"
-puts 5*8
 puts 6+6
 puts 8/3
 puts 9-3
@@ -9,4 +8,3 @@ gets
 puts first_name='kabi'
 puts last_name='eswaran'
 puts favorite_color='white'
-gets yur_first_name
